4f82e6d494aeb87280c701120ea87bd6f3308fd5
nipu56 <49304305+nipu56@users.noreply.github.com>
2022-05-19 13:49:12 +0900

    Update urls.py

--- winter/urls.py
+++ winter/urls.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 from django.urls import re_path, include
 
 urlpatterns = [
-    re_path('admin/', admin.site.urls),
-    re_path('', include('blog.urls')),
-    re_path('blog/', include('blog.urls', ), name='blog'),
-
+    re_path('admin/', admin.site.urls),# /admin/
+    
+    re_path(r'^$', HomeView.as_view(),name='home'),# /
+    re_path(r'^blog/', include('blog.urls', ), name='blog'), /blog/
+    
 ]
