076235dca2e5e51a052fa256a520905c08667e59
nipu56 <49304305+nipu56@users.noreply.github.com>
2022-05-19 13:49:27 +0900

    Update urls.py

--- winter/urls.py
+++ winter/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     re_path('admin/', admin.site.urls),# /admin/
     
     re_path(r'^$', HomeView.as_view(),name='home'),# /
-    re_path(r'^blog/', include('blog.urls', ), name='blog'), /blog/
+    re_path(r'^blog/', include('blog.urls', ), name='blog'), # /blog/
     
 ]
